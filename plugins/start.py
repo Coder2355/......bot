d15b2bd022f4dcc9392d504779ad2b267b674f9d
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-09-22 12:04:03 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -38,7 +38,7 @@ async def start_command(client: Client, message):
                 InlineKeyboardButton(
                     "🎗 Rᴇǫᴜᴇꜱᴛ Tᴏ Jᴏɪɴ Cʜᴀɴɴᴇʟ 2 🎗", url=client.link_two)
             ]] 
-            except Exception as e:
+        else except Exception as e:
                 print(e)
             try:
                 btn.append(
